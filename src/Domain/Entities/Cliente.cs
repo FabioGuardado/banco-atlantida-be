87125c9edeb8a2e48a839a8f3bbfc68b584dbee0
Fabio Guardado <fabio.guardado2012@gmail.com>
2024-01-27 22:36:49 -0600

    Agregar entidades del proyecto

---
+++ src/Domain/Entities/Cliente.cs
@@ -0,0 +1,9 @@
+﻿using BancoAtlantidaChallenge.Domain.Common;
+
+namespace BancoAtlantidaChallenge.Domain.Entities;
+public class Cliente : BaseEntity
+{
+    public string? Nombres { get; set; }
+    public string? Apellidos { get; set; }
+    public ICollection<TarjetaDeCredito>? TarjetasDeCredito { get; set; }
+}
