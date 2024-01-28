d4dc5832e06e80ad6ed4c67077bfb581ae6f9ece
Fabio Guardado <fabio.guardado2012@gmail.com>
2024-01-27 22:37:42 -0600

    Actualizar DbContext

--- src/Infrastructure/Data/ApplicationDbContext.cs
+++ src/Infrastructure/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using BancoAtlantidaChallenge.Application.Common.Interfaces;
+using BancoAtlantidaChallenge.Domain.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,7 +10,10 @@ public class ApplicationDbContext : DbContext, IApplicationDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
-    /* public DbSet<TodoList> TodoLists => Set<TodoList>(); */
+    public DbSet<Cliente> Clientes => Set<Cliente>();
+    public DbSet<TarjetaDeCredito> TarjetasDeCredito => Set<TarjetaDeCredito>();
+    public DbSet<Compra> Compras => Set<Compra>();
+    public DbSet<Pago> Pagos => Set<Pago>();
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
