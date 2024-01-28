c3a6702b9d968412b56646a0d76e5842ab4f162e
Fabio Guardado <fabio.guardado2012@gmail.com>
2024-01-27 22:38:12 -0600

    Agregar Configuracion de entidades

---
+++ src/Infrastructure/Data/Configurations/PagoConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using BancoAtlantidaChallenge.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BancoAtlantidaChallenge.Infrastructure.Data.Configurations;
+public class PagoConfiguration : IEntityTypeConfiguration<Pago>
+{
+    public void Configure(EntityTypeBuilder<Pago> builder)
+    {
+        builder.Property(p => p.NumeroDeAutorizacion).HasMaxLength(6);
+
+        builder.Property(p => p.Fecha).IsRequired();
+    }
+}
