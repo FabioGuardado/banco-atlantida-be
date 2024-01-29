714e646026ac571722cf0b3131fc88694e389787
Fabio Guardado <fabio.guardado2012@gmail.com>
2024-01-29 17:31:45 -0600

    Seed Compras Table

--- src/Infrastructure/Data/Configurations/CompraConfiguration.cs
+++ src/Infrastructure/Data/Configurations/CompraConfiguration.cs
@@ -10,5 +10,14 @@ public class CompraConfiguration : IEntityTypeConfiguration<Compra>
         builder.Property(c => c.NumeroDeAutorizacion).HasMaxLength(6);
 
         builder.Property(c => c.Fecha).IsRequired();
+
+        builder.HasData(
+            new Compra { Id = 1, NumeroDeAutorizacion = "551341", Descripcion = "Walmart", Monto = 12.43M, Fecha = new DateTime(2023, 12, 14), TarjetaDeCreditoId = 1 },
+            new Compra { Id = 2, NumeroDeAutorizacion = "454123", Descripcion = "Starbucks", Monto = 7.78M, Fecha = new DateTime(2024, 1, 1), TarjetaDeCreditoId = 1 },
+            new Compra { Id = 3, NumeroDeAutorizacion = "442152", Descripcion = "Recarga Tigo", Monto = 3.00M, Fecha = new DateTime(2023, 12, 28), TarjetaDeCreditoId = 1 },
+            new Compra { Id = 4, NumeroDeAutorizacion = "785424", Descripcion = "Recarga Tigo", Monto = 2.50M, Fecha = new DateTime(2024, 1, 12), TarjetaDeCreditoId = 1 },
+            new Compra { Id = 5, NumeroDeAutorizacion = "623121", Descripcion = "Farmacia San Nicolás", Monto = 54.00M, Fecha = new DateTime(2024, 1, 7), TarjetaDeCreditoId = 1 },
+            new Compra { Id = 6, NumeroDeAutorizacion = "323235", Descripcion = "Super Selectos", Monto = 34.76M, Fecha = new DateTime(2024, 1, 16), TarjetaDeCreditoId = 1 }
+        );
     }
 }
---
+++ src/Infrastructure/Data/Migrations/20240129233112_SeedComprasTable.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BancoAtlantidaChallenge.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedComprasTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Compras",
+                columns: new[] { "Id", "Descripcion", "Fecha", "Monto", "NumeroDeAutorizacion", "TarjetaDeCreditoId" },
+                values: new object[,]
+                {
+                    { 1, "Walmart", new DateTime(2023, 12, 14, 0, 0, 0, 0, DateTimeKind.Unspecified), 12.43m, "551341", 1 },
+                    { 2, "Starbucks", new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 7.78m, "454123", 1 },
+                    { 3, "Recarga Tigo", new DateTime(2023, 12, 28, 0, 0, 0, 0, DateTimeKind.Unspecified), 3.00m, "442152", 1 },
+                    { 4, "Recarga Tigo", new DateTime(2024, 1, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), 2.50m, "785424", 1 },
+                    { 5, "Farmacia San Nicolás", new DateTime(2024, 1, 7, 0, 0, 0, 0, DateTimeKind.Unspecified), 54.00m, "623121", 1 },
+                    { 6, "Super Selectos", new DateTime(2024, 1, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), 34.76m, "323235", 1 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Compras",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Compras",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Compras",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Compras",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Compras",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Compras",
+                keyColumn: "Id",
+                keyValue: 6);
+        }
+    }
+}
