8ec7889c8a3c8d9da21deddd953d1a98397848bd
Fabio Guardado <fabio.guardado2012@gmail.com>
2024-01-29 17:33:54 -0600

    Seed Pagos Table

--- src/Infrastructure/Data/Configurations/PagoConfiguration.cs
+++ src/Infrastructure/Data/Configurations/PagoConfiguration.cs
@@ -10,5 +10,10 @@ public class PagoConfiguration : IEntityTypeConfiguration<Pago>
         builder.Property(p => p.NumeroDeAutorizacion).HasMaxLength(6);
 
         builder.Property(p => p.Fecha).IsRequired();
+
+        builder.HasData(
+            new Pago { Id = 1, NumeroDeAutorizacion = "005185", Descripcion = "Pago de TC", Abono = 3.00M, Fecha = new DateTime(2024, 1, 9), TarjetaDeCreditoId = 1 },
+            new Pago { Id = 2, NumeroDeAutorizacion = "224235", Descripcion = "Pago de TC", Abono = 12.43M, Fecha = new DateTime(2024, 1, 14), TarjetaDeCreditoId = 1 }
+        );
     }
 }
