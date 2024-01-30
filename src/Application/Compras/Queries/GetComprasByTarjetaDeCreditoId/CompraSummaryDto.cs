6887ff0bf5e5db17f4792385929374f28bfdeb21
Fabio Guardado <fabio.guardado2012@gmail.com>
2024-01-30 15:28:55 -0600

    Agregar servicios para obtener compras

---
+++ src/Application/Compras/Queries/GetComprasByTarjetaDeCreditoId/CompraSummaryDto.cs
@@ -0,0 +1,19 @@
+﻿using BancoAtlantidaChallenge.Domain.Entities;
+
+namespace BancoAtlantidaChallenge.Application.Compras.Queries.GetComprasByTarjetaDeCreditoId;
+public class CompraSummaryDto
+{
+    public int Id { get; set; }
+    public string? NumeroDeAutorizacion { get; set; }
+    public DateTime? Fecha { get; set; }
+    public string? Descripcion { get; set; }
+    public decimal Monto { get; set; }
+
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<Compra, CompraSummaryDto>();
+        }
+    }
+}
---
+++ src/Application/Compras/Queries/GetComprasByTarjetaDeCreditoId/GetComprasByTarjetaDeCreditoId.cs
@@ -0,0 +1,34 @@
+﻿using BancoAtlantidaChallenge.Application.Common.Interfaces;
+using BancoAtlantidaChallenge.Application.Common.Mappings;
+
+namespace BancoAtlantidaChallenge.Application.Compras.Queries.GetComprasByTarjetaDeCreditoId;
+public record GetComprasByTarjetaDeCreditoIdQuery : IRequest<IEnumerable<CompraSummaryDto>>
+{
+    public int Id { get; set; }
+
+    public GetComprasByTarjetaDeCreditoIdQuery(int id)
+    {
+        Id = id;
+    }
+
+    public class GetComprasByTarjetaDeCreditoId : IRequestHandler<GetComprasByTarjetaDeCreditoIdQuery, IEnumerable<CompraSummaryDto>>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetComprasByTarjetaDeCreditoId(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<CompraSummaryDto>> Handle(GetComprasByTarjetaDeCreditoIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Compras
+                .Where(c => c.TarjetaDeCreditoId == request.Id)
+                .Where(c => c.Fecha >= DateTime.Today.AddMonths(-1))
+                .OrderByDescending(c => c.Fecha)
+                .ProjectToListAsync<CompraSummaryDto>(_mapper.ConfigurationProvider);
+        }
+    }
+}
