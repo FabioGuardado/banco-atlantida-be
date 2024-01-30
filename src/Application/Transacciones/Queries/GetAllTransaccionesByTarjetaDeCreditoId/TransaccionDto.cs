968d9adf545f777df860260b4087ff40b896468b
Fabio Guardado <fabio.guardado2012@gmail.com>
2024-01-30 15:29:13 -0600

    Agregar servicios para obtener transacciones

---
+++ src/Application/Transacciones/Queries/GetAllTransaccionesByTarjetaDeCreditoId/TransaccionDto.cs
@@ -0,0 +1,23 @@
+﻿using BancoAtlantidaChallenge.Application.Compras.Queries.GetComprasByTarjetaDeCreditoId;
+using BancoAtlantidaChallenge.Domain.Entities;
+
+namespace BancoAtlantidaChallenge.Application.Transacciones.Queries.GetAllTransaccionesByTarjetaDeCreditoId;
+public class TransaccionDto
+{
+    public int Id { get; set; }
+    public string? NumeroDeAutorizacion { get; set; }
+    public DateTime? Fecha { get; set; }
+    public string? Descripcion { get; set; }
+    public decimal? Abono { get; set; }
+    public decimal? Monto { get; set; }
+
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<Compra, TransaccionDto>();
+            CreateMap<Pago, TransaccionDto>();
+            CreateMap<TransaccionDto, TransaccionDto>();
+        }
+    }
+}
